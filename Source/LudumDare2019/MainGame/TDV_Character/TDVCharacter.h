b0deeeb4322be5b849c481b978e32f195c4956b4
Raineri Dino <dino.raineri@gmail.com>
2019-04-13 20:44:47 -0400

    Add top down view character
    Add top down character c++ and blueprint
    Integrate anim from topdown example project.

---
+++ Source/LudumDare2019/MainGame/TDV_Character/TDVCharacter.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TDVCharacter.generated.h"
+
+
+UCLASS(config = Game)
+class ATDVCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+};
